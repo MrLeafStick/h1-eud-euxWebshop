63efc3e42f97627021670190dfee5038566b3159
kenenthLovgren <kenneth.l.thomsen@gmail.com>
2021-06-08 13:39:59 +0200

    Reform the project

---
+++ BlazeWebShop/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazeWebShop.Models
+{
+    
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Addrees { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
